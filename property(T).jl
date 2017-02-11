a060b4ed8b6570e77cba3c81010960760f5bed8b
kalmar <kalmar@amu.edu.pl>
2017-02-11 13:43:18 +0100

    remove @everywhere as it is exported twice (producing warnings)

--- property(T).jl
+++ property(T).jl
@@ -92,7 +92,7 @@ function EOI{T<:Number}(Δ::GroupAlgebraElement{T}, κ::T)
     return Δ*Δ - κ*Δ
 end
 
-@everywhere function square(vector, elt)
+function square_as_elt(vector, elt)
     zzz = zeros(elt.coefficients)
     zzz[1:length(vector)] = vector
 #     new_base_elt = GroupAlgebraElement(zzz, elt.product_matrix)
