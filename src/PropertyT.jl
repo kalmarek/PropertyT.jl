69e74190af1efcbd1b2d2c66df31dd2a3f0b8106
kalmar <kalmar@amu.edu.pl>
2017-03-31 22:34:56 +0200

    tad better try...catch syntax

--- src/PropertyT.jl
+++ src/PropertyT.jl
@@ -124,13 +124,11 @@ function compute_κandA(sdp_constraints, Δ::GroupAlgebraElement, solver::Abstra
     t = @timed SDP_problem = create_SDP_problem(sdp_constraints, Δ; upper_bound=upper_bound)
     info(logger, timed_msg(t))
 
-    κ = 0.0
-    A = nothing
     while κ == 0.0
-        try
-            κ, A = solve_SDP(SDP_problem, solver)
+        κ, A = try
+            solve_SDP(SDP_problem, solver)
         catch y
-            warn(solver_logger, y)
+            warn(logger, y)
         end
     end
     return κ, A
