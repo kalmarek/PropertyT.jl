1f5e7ebdbe0d6ea144561bba62240a673788b16b
kalmarek <kalmar@amu.edu.pl>
2019-02-21 14:53:11 +0100

    fix performance regression in generating constraints

--- src/sos_sdps.jl
+++ src/sos_sdps.jl
@@ -100,7 +100,7 @@ end
 
 function constraintLHS!(M, cnstr, Us, Ust, dims, eps=1000*eps(eltype(first(M))))
     for π in 1:lastindex(Us)
-        M[π] = dims[π].*PropertyT.clamp_small!(Ust[π]*cnstr*Us[π], eps)
+        M[π] = dims[π].*PropertyT.clamp_small!(Ust[π]*(cnstr*Us[π]), eps)
     end
 end
 
