0778f828e4bade2afd55d27eb81e7bc13c5389b6
kalmarek <kalmar@amu.edu.pl>
2020-06-23 16:13:42 +0200

    rework group actions

--- test/actions.jl
+++ test/actions.jl
@@ -3,12 +3,9 @@
     ssgs(M::MatAlgebra, i, j) = (S = [Eij(M, i, j), Eij(M, j, i)];
         S = unique([S; inv.(S)]); S)
 
-    rmul = Groups.rmul_autsymbol
-    lmul = Groups.lmul_autsymbol
-
     function ssgs(A::AutGroup, i, j)
-        rmuls = [rmul(i,j), rmul(j,i)]
-        lmuls = [lmul(i,j), lmul(j,i)]
+        rmuls = [Groups.transvection_R(i,j), Groups.transvection_R(j,i)]
+        lmuls = [Groups.transvection_L(i,j), Groups.transvection_L(j,i)]
         gen_set = A.([rmuls; lmuls])
         return unique([gen_set; inv.(gen_set)])
     end
@@ -33,7 +30,7 @@
         elt2 = E_R[rand(sizes[1]:sizes[2])]
         y = 2RG(elt2) - RG(elt)
 
-        for G in [PermGroup(N), WreathProduct(PermGroup(2), PermGroup(N))]
+        for G in [SymmetricGroup(N), WreathProduct(SymmetricGroup(2), SymmetricGroup(N))]
             @test all(g(one(M)) == one(M) for g in G)
             @test all(rdict[g(m)] <= sizes[1] for g in G for m in S)
             @test all(g(m)*g(n) == g(m*n) for g in G for m in S for n in S)
@@ -50,9 +47,9 @@
             Sij = ssgs(M, i,j)
             Δij= PropertyT.spLaplacian(RG, Sij)
 
-            @test all(p(Δij) == PropertyT.spLaplacian(RG, ssgs(M, p[i], p[j])) for p in PermGroup(N))
+            @test all(p(Δij) == PropertyT.spLaplacian(RG, ssgs(M, p[i], p[j])) for p in SymmetricGroup(N))
 
-            @test all(g(Δij) == PropertyT.spLaplacian(RG, ssgs(M, g.p[i], g.p[j])) for g in WreathProduct(PermGroup(2), PermGroup(N)))
+            @test all(g(Δij) == PropertyT.spLaplacian(RG, ssgs(M, g.p[i], g.p[j])) for g in WreathProduct(SymmetricGroup(2), SymmetricGroup(N)))
         end
     end
 end
@@ -79,7 +76,7 @@ end
         elt2 = E_R[rand(sizes[1]:sizes[2])]
         y = 2RG(elt2) - RG(elt)
 
-        for G in [PermGroup(N), WreathProduct(PermGroup(2), PermGroup(N))]
+        for G in [SymmetricGroup(N), WreathProduct(SymmetricGroup(2), SymmetricGroup(N))]
             @test all(g(one(M)) == one(M) for g in G)
             @test all(rdict[g(m)] <= sizes[1] for g in G for m in S)
             @test all(g(m)*g(n) == g(m*n) for g in G for m in S for n in S)
@@ -95,9 +92,9 @@ end
         Sij = ssgs(M, i,j)
         Δij= PropertyT.spLaplacian(RG, Sij)
 
-        @test all(p(Δij) == PropertyT.spLaplacian(RG, ssgs(M, p[i], p[j])) for p in PermGroup(N))
+        @test all(p(Δij) == PropertyT.spLaplacian(RG, ssgs(M, p[i], p[j])) for p in SymmetricGroup(N))
 
-        @test all(g(Δij) == PropertyT.spLaplacian(RG, ssgs(M, g.p[i], g.p[j])) for g in WreathProduct(PermGroup(2), PermGroup(N)))
+        @test all(g(Δij) == PropertyT.spLaplacian(RG, ssgs(M, g.p[i], g.p[j])) for g in WreathProduct(SymmetricGroup(2), SymmetricGroup(N)))
     end
 end
 
