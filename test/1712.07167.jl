8d23fd3a736905643fd23ab58df22549a17b5b12
kalmarek <kalmar@amu.edu.pl>
2020-10-18 01:08:42 +0200

    tweak max_iters in tests

--- test/1712.07167.jl
+++ test/1712.07167.jl
@@ -70,7 +70,7 @@
         @test λ == PropertyT.spectral_gap(sett)
         @test PropertyT.check_property_T(sett) == false
 
-        sett = PropertyT.Settings(NAME, G, S, autS, with_SCS(2000, accel=20, warm_start=true);
+        sett = PropertyT.Settings(NAME, G, S, autS, with_SCS(5000, accel=20, warm_start=true);
         upper_bound=1.3, force_compute=true)
 
         @info sett
